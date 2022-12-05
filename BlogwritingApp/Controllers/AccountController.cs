b88e8cb34738fae8f1942808c28b9e590506802c
DevashishRShejao <119856687+DevashishRShejao@users.noreply.github.com>
2022-12-05 17:47:38 +0530

    Add files via upload

---
+++ BlogwritingApp/Controllers/AccountController.cs
@@ -0,0 +1,87 @@
+﻿using BlogwritingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+
+namespace BlogwritingApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        // public ActionResult Index()
+        //{
+        //  return View();
+        //}
+        [AllowAnonymous]
+        public ActionResult Index() => View();
+        [HttpGet]
+        public ActionResult Login(string content)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(UserTable postedData)
+        {
+           
+                var context = new BlogDatabaseEntities();
+                var user = context.UserTables.SingleOrDefault(rec => rec.UserName == postedData.UserName && rec.UserPassword == postedData.UserPassword);
+                if (user != null)
+                {
+                    Session["currentUser"] = user; //To store it till the User logs off!!!
+                    //FormsAuthentication.RedirectFromLoginPage(UserName, true);
+                    return RedirectToAction("Index","Account");
+                }
+                else
+                {
+                    ViewBag.ErrorInfo = "Login has failed!";
+                    return View();
+                }
+            }
+
+        public ActionResult RegisterNew()
+        {
+            var userDetails = new UserTable();
+            return View(userDetails);
+        }
+        [HttpPost]
+        public ActionResult RegisterNew(UserTable postedData)
+        {
+            if (ModelState.IsValid && isValidUser(postedData.UserName))
+            {
+                var context = new BlogDatabaseEntities();
+                var userTableRow = new UserTable
+                {
+                    UserEmailAddress = postedData.UserEmailAddress,
+                    UserPassword = postedData.UserPassword,
+                    UserName = postedData.UserName,
+                    Address=postedData.Address
+                };
+                context.UserTables.Add(userTableRow);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("Failuers", "User Already Exists");
+                return View();
+            }
+        }
+
+        private bool isValidUser(string userName)
+        {
+            var context = new BlogDatabaseEntities();
+            var selected = context.UserTables.FirstOrDefault((user) => user.UserName == userName);
+            return selected == null ? true : false;
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BlogwritingApp/Controllers/BlogController.cs
@@ -0,0 +1,122 @@
+﻿using BlogwritingApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogwritingApp.Controllers
+{
+    [Authorize]
+    public class BlogController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult AllBlogs()
+        {
+            var context = new BlogDatabaseEntities();
+            var data = context.BlogTables.ToList();
+            return PartialView(data);
+        }
+        [AllowAnonymous]
+        public ActionResult AllEditableBlogs()
+        {
+            if (Session["currentUser"] == null)
+            {
+                ViewBag.ErrorInfo = "Please login Before U Post a Blog!!!";
+                return PartialView(null);
+            }
+            var user = Session["currentUser"] as UserTable;
+            var context = new BlogDatabaseEntities();
+            var blogs = context.BlogTables.Where(b => b.UserId == user.UserId).ToList();
+            return PartialView(blogs);
+        }
+
+        [AllowAnonymous]
+        public ActionResult AddNew()
+        {
+            if (Session["currentUser"] == null)
+            {
+                TempData["ErrorInfo"] = "Please login Before U Post a Blog!!!";
+                TempData.Keep();
+                return RedirectToAction("Login", "Account");
+            }
+            var user = Session["currentUser"] as UserTable;
+            var blog = new BlogTable();
+            blog.UserId = user.UserId;
+            blog.UserTable = user;
+            return View(blog);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult AddNew(BlogTable blog)
+        {
+            var context = new BlogDatabaseEntities();
+            context.BlogTables.Add(blog);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Account");
+        }
+
+        //public ActionResult UpdateRec(BlogTable postedData)
+        //{
+        //    var context = new BlogDatabaseEntities();
+        //    var model = context.BlogTables.Where((blog) => blog.UserId == postedData.UserId).FirstOrDefault();
+        //    if (model == null) throw new Exception("This is not found to update");
+        //    model.Title = postedData.Title;
+        //    model.Image = postedData.Image;
+        //    model.Details = postedData.Details;
+        //    context.SaveChanges();
+        //    return RedirectToAction();
+        //}
+        [AllowAnonymous]
+        public ActionResult EditBlogs(string Id)
+        {
+            var context = new BlogDatabaseEntities();
+            var BId = int.Parse(Id);
+            var selected = context.BlogTables.SingleOrDefault(b => b.BlogId == BId);
+            return View(selected);
+        }
+        
+        [AllowAnonymous]
+       // [HttpPost]
+        public ActionResult UpdateBlogs(BlogTable blogsTable)
+        {
+            var context = new BlogDatabaseEntities();
+            var selected = context.BlogTables.SingleOrDefault(b => b.BlogId == blogsTable.BlogId);
+            if (selected == null)
+            {
+                throw new System.Exception("Blog is not found");
+            }
+            selected.Title = blogsTable.Title;
+            selected.UserId = blogsTable.UserId;
+            selected.Image = blogsTable.Image;
+            selected.Details = blogsTable.Details;
+            
+            context.SaveChanges();
+            return RedirectToAction("Index","Account");
+        }
+        [AllowAnonymous]
+        public ActionResult Delete(string id)
+        {
+            var context = new BlogDatabaseEntities();
+            var BId = int.Parse(id);
+            var model = context.BlogTables.Where((blog) => blog.BlogId == BId).FirstOrDefault();//SELECT * From EmpTable where Id = empId;
+            context.BlogTables.Remove(model);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Account");
+        }
+        [AllowAnonymous]
+        public ActionResult userBlogs()
+        {
+            if (Session["currentUser"] == null)
+            {
+                TempData["ErrorInfo"] = "Please login Before U Post a Blog!!!";
+                TempData.Keep();
+                return RedirectToAction("Login", "Account");
+            }
+            var context = new BlogDatabaseEntities();
+            var data = context.BlogTables.ToList();
+            return View(data);
+        }
+    }
+}
